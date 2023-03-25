45653b83da9704bfc9f3b83e0b3f165185091769
Jack Banks <jackb4@illinois.edu>
2023-03-25 14:21:45 -0500

    jack's paths for loading data

--- R/load-data-jack.R
+++ R/load-data-jack.R
@@ -2,3 +2,5 @@
 macro = readr::read_csv("I:/datathon-data/macro_data.csv")
 forecast = readr::read_csv("I:/datathon-data/forecast_starting_data.csv")
 training = readr::read_csv("I:/datathon-data/training_data.csv")
+
+
