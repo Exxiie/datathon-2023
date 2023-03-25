e499035d13ab8f9eeb868af954d58877964a7e8e
Jack Banks <jackb4@illinois.edu>
2023-03-24 23:45:29 -0500

    first commit

---
+++ R/load-data-jack.R
@@ -0,0 +1,4 @@
+
+macro = readr::read_csv("I:/datathon-data/macro_data.csv")
+forecast = readr::read_csv("I:/datathon-data/forecast_starting_data.csv")
+training = readr::read_csv("I:/datathon-data/training_data.csv")
